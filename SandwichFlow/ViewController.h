405cdef236664c688a86786986f7989933b30eff
nathanchen <qche7764@uni.sydney.edu.au>
2015-03-14 20:53:40 +0800

    Initial Commit

---
+++ SandwichFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SandwichFlow
+//
+//  Created by Nate on 14/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SandwichFlow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SandwichFlow
+//
+//  Created by Nate on 14/03/2015.
+//  Copyright (c) 2015 Nathan CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
